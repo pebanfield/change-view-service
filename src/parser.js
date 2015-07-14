53dfa4f3380ace0d572d20c8c326632c7b859263
P.E. Banfield <pebanfield@gmail.com>
2015-07-14 09:48:42 -0700

    added new status logic and test cases

--- src/parser.js
+++ src/parser.js
@@ -10,13 +10,14 @@ var api = {getHistory: _getHistory};
 
 function _getHistory(){
 
-  var blobTable = {};
+  var entryTable = {};
   var open = nodegit.Repository.open;
 
 // Open the repository directory.
-  return open("/Users/pebanfield/PROJECTS/change-view/change-viewer")
+  return open("test/data")
     // Open the master branch.
     .then(function(repo) {
+
       return repo.getMasterCommit();
     })
     // Display information about commits on master.
@@ -27,45 +28,71 @@ function _getHistory(){
 
       // Listen for commit events from the history.
       history.on('commit', function(commit) {
-
       });
 
       history.on('end', function(commits){
 
         var responseCommits = [];
+        var commitTreeCalls = [];
+
         commits.forEach(function(commit){
+
           var commitObj = {};
           commitObj.author = commit.author().name();
           commitObj.revision = commit.sha();
           commitObj.date = commit.date();
           commitObj.message = commit.message();
           commitObj.entries = [];
-          commit.getTree().then(function(tree) {
-            // Use treeEntry
-            var entries = tree.entries();
-            entries.forEach(function(entry){
+          commitTreeCalls.push(commit.getTree());
+          responseCommits.push(commitObj);
+        });
+
+        Promise.all(commitTreeCalls).then(function(commitTrees){
+
+          commitTrees.forEach(function(commitTree, commitIndex){
+
+            var entries = commitTree.entries();
+
+            entries.forEach(function(entry, entryIndex){
               var entryObj = {};
+              try {
+                entryObj.path = entry.path();
+                var nameArray = entry.path().split("/");
+                entryObj.name = nameArray[nameArray.length-1];
+                entryObj.author = responseCommits[commitIndex].author;
+                entryObj.date = responseCommits[commitIndex].date;
+              } catch(error) {
+                console.log("error : " + error);
+              }
+
               if(entry.isFile()){
+
                 entry.getBlob().then(function(blob){
-                  entryObj.size = blob.rawsize();
-                  entryObj.date = commit.date();
-                  entryObj.kind = 'file';
+
+                  try {
+                    entryObj.size = blob.rawsize();
+                    entryObj.kind = 'file';
+                    entryObj.mode = blob.filemode();
+                  } catch(error){
+                    console.log("error : " + error);
+                  }
+                  if(entryIndex === entries.length-1 && commitIndex === commitTrees.length-1){
+
+                    responseCommits.reverse(); //prints history with the most recent commit last
+                    _setStatus(responseCommits, entryTable);
+                    resolver.resolve(responseCommits);
+                  }
                 });
               } else {
                 entryObj.kind = 'tree';
               }
-              entryObj.path = entry.path();
-              var nameArray = entry.path().split("/");
-              entryObj.name = nameArray[nameArray.length-1];
-              entryObj.time = commit.date();
-              entryObj.author = commit.author().name();
-              commitObj.entries.push(entryObj);
-            });
-          });
-          responseCommits.push(commitObj);
-        });
 
-        resolver.resolve(responseCommits);
+              responseCommits[commitIndex].entries.push(entryObj);
+            });
+
+          });
+
+        });
       });
 
       // Start emitting events.
@@ -73,6 +100,45 @@ function _getHistory(){
 
       return resolver.promise;
     });
+}
+
+function _setStatus(commits, entryTable){
+
+  commits.forEach(function(commit){
+
+    commit.entries.forEach(function(entry){
+
+      var previousEntry = entryTable[entry.path];
+      if(previousEntry){
+
+        if(entry.date != previousEntry.date){
+          entry.status = 'modified';
+        }
+      } else {
+        entry.status = 'added';
+        entryTable[entry.path] = entry;
+      }
+
+    });
+    //identify deleted entries
+    for (var property in entryTable) {
+      if (entryTable.hasOwnProperty(property)) {
+        var exists = false;
+        var currentEntry = null;
+        commit.entries.forEach(function(entry){
+          currentEntry = entry;
+          if(entry.path === entryTable[property].path){
+            exists = true;
+          }
+        });
+        if(!exists){
+          entryTable[property].status = 'deleted';
+          commit.entries.push(entryTable[property]);
+          delete entryTable[property];
+        }
+      }
+    }
+  });
 }
 
 module.exports = api;
--- test/parser-spec.js
+++ test/parser-spec.js
@@ -8,10 +8,33 @@ describe('node-git parser', function(){
 
   describe('getHistory', function(done){
 
-    it('should return a git log history', function(done){
+    it('should return a git log history as a json object', function(done){
 
       parser.getHistory().then(function(commits){
         assert.ok(commits);
+        done();
+      });
+    });
+
+    it('should return single commit json objects with latest first', function(done){
+
+      parser.getHistory().then(function(commits){
+        commits.reverse();
+        var commitOne = commits[0];
+
+        assert.equal(commitOne.revision, '80d2d5bd3bb75cd7978caf1cebba5d6264b72dcd');
+        done();
+      });
+    });
+
+    it('should return commit entry status', function(done){
+
+      parser.getHistory().then(function(commits){
+
+        var commitOne = commits[0];
+        var commitTwo = commits[1];
+        assert.equal(commitOne.entries[0].status, 'added');
+        assert.equal(commitTwo.entries[1].status, 'deleted');
         done();
       });
     });
